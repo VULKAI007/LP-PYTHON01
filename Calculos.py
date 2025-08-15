7e70110153a58cdd0c7f9daeba10c78f34314ebb
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:23 -0300

    feat: tipagem e operacoes matematicas

---
+++ Calculos.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+num_um = float(input("DIGITE O PRIMEIRO NÚMERO: "))
+num_dois = float(input("DIGITE O SEGUNDO NÚMERO: "))
+
+soma = num_um + num_dois
+subtracao = num_um - num_dois
+multiplicacao = num_um * num_dois
+divisao = num_um / num_dois
+
+
+print(f"SOMA:  {soma}")
+print(f"SUBTRAÇÃO:  {subtracao}")
+print(f"MULTIPLICAÇÃO:  {multiplicacao}")
+print(f"DIVISÃO:  {divisao}")
+
+
+
---
+++ Teste.py
@@ -0,0 +1,14 @@
+#TIPAGEM DINÂMICA
+#O COMANDO (import os os.system), serve para limpar o terminal 
+
+import os
+os.system("cls")
+
+nome = input("DIGITE SEU NOME: ")
+idade = input("DIGITE SEU NOME: ")
+
+
+print(f"NOME: {nome}")
+print(f"IDADE: {idade}")
+
+
